864bb7e19eff484f62a9899f6bb33d7765e18c31
NNilawan <nilawan_l@cmu.ac.th>
2021-10-04 04:14:53 +0700

    Q7 Fix

--- src/main/java/se331/lab/rest/controller/UserController.java
+++ src/main/java/se331/lab/rest/controller/UserController.java
@@ -1,11 +1,13 @@
 package se331.lab.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import se331.lab.rest.security.entity.User;
+import se331.lab.rest.security.repository.UserRepository;
 import se331.lab.rest.service.UserService;
 import se331.lab.rest.util.LabMapper;
 
@@ -13,9 +15,15 @@ import se331.lab.rest.util.LabMapper;
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    UserRepository userRepository;
     @PostMapping("/registers")
     public ResponseEntity<?> addUser(@RequestBody User user) {
-        User output = userService.save(user);
-        return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(output));
+        if (userRepository.findByUsername(user.getUsername()) == null ) {
+            User output = userService.save(user);
+            return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(output));
+        }else {
+            return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.BAD_GATEWAY);
+        }
     }
 }
